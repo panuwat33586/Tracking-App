81ba0242b258702b16c9b0e990849f4bf096b4e6
panuwat33586 <panuwat33586@gmail.com>
2020-03-01 17:45:02 +0700

    using jwt to create and verify token for authorization

--- Tracking-Server/src/routes/authRoutes.js
+++ Tracking-Server/src/routes/authRoutes.js
@@ -1,14 +1,21 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const User =mongoose.model('User')
+const express = require('express')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+const jwt =require('jsonwebtoken')
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/signup',async(req,res)=>{
-    const {email,password}=req.body;
-    const user=new User({email,password});
-    await user.save()
-    res.send('You made a post request')
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = new User({ email, password });
+        await user.save()
+        const token =jwt.sign({userId:user._id},'MY_SECRET_KEY')
+        res.send({token})
+    } catch (err) { 
+        return res.status(422).send(err.message)
+    }
+
 })
 
-module.exports=router;
+module.exports = router;
