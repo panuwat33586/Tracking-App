9f1c6563de9ad55de503e8beea60e786dd144a51
panuwat33586 <panuwat33586@gmail.com>
2020-03-04 14:35:37 +0700

    setup screens and navigation flow

---
+++ TrackApp/src/screens/AccountScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+
+const AccountScreen=()=>{
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+
+})
+
+export default AccountScreen
---
+++ TrackApp/src/screens/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+
+const SignupScreen=()=>{
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+
+})
+
+export default SignupScreen
