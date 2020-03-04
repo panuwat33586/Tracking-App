c45b4d632a87a223cd629236a07bcf0555004e08
panuwat33586 <panuwat33586@gmail.com>
2020-03-04 14:41:28 +0700

    install react-navigation and import React to App.js

--- TrackApp/App.js
+++ TrackApp/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
