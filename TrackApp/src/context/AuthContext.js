6869cd6ce557d5392ab38c893b2c5d0cd8382aea
panuwat33586 <panuwat33586@gmail.com>
2020-03-05 23:38:14 +0700

    makesiginscreen work

--- TrackApp/src/context/AuthContext.js
+++ TrackApp/src/context/AuthContext.js
@@ -1,44 +1,54 @@
-import {AsyncStorage} from 'react-native'
+import { AsyncStorage } from 'react-native'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
-import {navigate} from '../navigationRef'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload }
-    case 'signup':
-      return {errorMessage:'',token:action.payload}
+    case 'signin':
+      return { errorMessage: '', token: action.payload }
     default:
       return state;
   }
 };
 
-const signup = dispatch =>  async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post('/signup', { email, password })
-      await AsyncStorage.setItem('token',response.data.token)
-      dispatch({type:'signup',payload:response.data.token})
-      navigate('TrackList')
-    } catch (err) {
-      dispatch({ type: 'add_error', payload: 'something went wrong with signup' })
-    }
-  };
-
-
-const signin = dispatch =>({ email, password }) => {
-
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password })
+    await AsyncStorage.setItem('token', response.data.token)
+    dispatch({ type: 'signin', payload: response.data.token })
+    navigate('TrackList')
+  } catch (err) {
+    dispatch({ type: 'add_error', payload: 'something went wrong with signup' })
   }
+};
+
+
+const signin = dispatch =>async ({ email, password }) => {
+  try {
+     const response= await trackerApi.post('/signin', { email, password })
+     await AsyncStorage.setItem('token', response.data.token)
+     dispatch({type:'signin',payload:response.data.token})
+     navigate('TrackList')
+  } catch (err) {
+       dispatch({
+         type:'add_error',
+         payload:'Somthing went wrong with signin'
+       })
+  }
+}
 
 
 const signout = dispatch => () => {
 
-  }
+}
 
 
 
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { token:null, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
--- TrackApp/src/screens/SigninScreen.js
+++ TrackApp/src/screens/SigninScreen.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import {View,Text,StyleSheet} from 'react-native'
+import React,{useContext} from 'react'
+import {View,StyleSheet} from 'react-native'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+import {Context as AuthContext} from '../context/AuthContext'
 
 const SigninScreen=()=>{
+    const {state,signin}=useContext(AuthContext)
     return(
-        <View>
-
+        <View style={styles.container}>
+           <AuthForm
+           headerText='Sign in to your account'
+           errorMessage=''
+           onSubmit={signin}
+           submitButtonText='Sign In'
+           />
+           <NavLink
+           text="Don't have an account sign up instead"
+           routeName='Signup'
+           />
         </View>
     )
 }
+SigninScreen.navigationOptions = () => {
+    return {
+      headerShown: false
+    }
+  }
 
-const styles=StyleSheet.create({
-
-})
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      marginBottom: 200
+    }
+  })
 
 export default SigninScreen
