ff169a6e61daca0176f09817694db7368c6fcca6
panuwat33586 <panuwat33586@gmail.com>
2020-03-06 16:46:32 +0700

    create a fake location function

--- TrackApp/src/screens/TrackCreateScreen.js
+++ TrackApp/src/screens/TrackCreateScreen.js
@@ -1,14 +1,35 @@
-import React from 'react'
+// import '../mockLocation'
+import React,{useEffect,useState} from 'react'
 import {StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-navigation'
 import {Text} from 'react-native-elements'
+import {requestPermissionsAsync, watchPositionAsync, Accuracy} from 'expo-location'
 import Map from '../components/Map'
 
 const TrackCreateScreen=()=>{
+    const [err,setErr] =useState(null)
+    const startWatching=async()=>{
+        try{
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy:Accuracy.BestForNavigation,
+                timeInterval:1000,
+                distanceInterval:10
+            },(location)=>{
+               console.log(location)
+            })
+        }catch(e){
+              setErr(e)
+        }
+    }
+    useEffect(() => {
+       startWatching()
+    }, [])
     return(
         <SafeAreaView forceInset={{top:"always"}}>
              <Text h2>Create a Track</Text>
              <Map/>
+             {err?<Text h4>Please enable services location</Text>:null}
         </SafeAreaView>
     )
 }
