8ffcdbe785fc02d5d6937f0dd177ef09f2d5fa39
panuwat33586 <panuwat33586@gmail.com>
2020-04-21 21:36:40 +0700

    create automate token adder to header on track.js api

--- TrackApp/src/api/tracker.js
+++ TrackApp/src/api/tracker.js
@@ -1,5 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
+import {AsyncStorage} from 'react-native';
 
-export default axios.create({
-    baseURL:' http://b145ec50.ngrok.io'
-})
+const instance= axios.create({
+    baseURL:'  http://6335b5f4.ngrok.io'
+});
+
+instance.interceptors.request.use(
+    async (config)=>{
+        const token = await AsyncStorage.getItem('token')
+        if(token){
+            config.headers.Authorization=`Bearer ${token}`;
+        }
+        return config;
+    },
+    (err)=>{
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
