c2eeb933653075ca5622ecd97b8681c1008b6504
panuwat33586 <panuwat33586@gmail.com>
2020-04-21 22:52:55 +0700

    fix database longitude name

--- TrackApp/src/screens/TrackListScreen.js
+++ TrackApp/src/screens/TrackListScreen.js
@@ -1,31 +1,35 @@
-import React,{useContext} from 'react';
-import {Text,StyleSheet,FlatList,TouchableOpacity} from 'react-native';
-import {NavigationEvents} from 'react-navigation';
-import {ListItem} from 'react-native-elements';
-import {Context as TrackContext} from '../context/TrackContext';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackListScreen=({navigation})=>{
+const TrackListScreen = ({ navigation }) => {
 
-    const {state,fetchTracks}=useContext(TrackContext)
+    const { state, fetchTracks } = useContext(TrackContext)
 
-    return(
+    return (
         <>
-        <NavigationEvents onWillFocus={()=>fetchTracks()}/>
-           <Text style={{fontSize:48}}>TrackList Screen</Text>
-           <FlatList
-           data={state}
-           keyExtractor={item=>item._id}
-           renderItem={({item})=>{
-                return <TouchableOpacity>
-                    <ListItem chevron title={item.name}/>
-                </TouchableOpacity>
-           }}
-           />
+            <NavigationEvents onWillFocus={() => fetchTracks()} />
+            <Text style={{ fontSize: 48 }}>TrackList Screen</Text>
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={()=>
+                            navigation.navigate('TrackDetail',{_id:item._id})
+                        }>
+                            <ListItem chevron title={item.name} />
+                        </TouchableOpacity>
+                    )
+                }}
+            />
         </>
     )
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
 
 })
 
